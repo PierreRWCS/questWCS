47f6bd209f1d76f8475b0ed5856215ea540f6c2d
PierreRWCS <61827591+PierreRWCS@users.noreply.github.com>
2020-09-05 17:20:27 +0200

    Add files via upload

---
+++ pop.js
@@ -0,0 +1,8 @@
+/* pop-up */
+
+const NameMovie = 'Les Vacances de monsieur Hulot ';
+const annéeSort = ' année de sortie 1953';
+const réal = 'Jacques Tati';
+const message = 'salut ^^ mon filme favori  est' + NameMovie + 'de ' + réal+ 'sortie en' +annéeSort+ 'exelent je te le conseille ';
+
+alert (message);
